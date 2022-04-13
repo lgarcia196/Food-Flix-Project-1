62d27e65d1b4d25b09c10734dbc6ab3926ba992b
Cody Polland <cody.polland@gmail.com>
2022-04-13 14:20:19 -0700

    displaying the movie details finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,11 +5,21 @@ fetch('https://imdb-api.com/en/API/Top250Movies/k_pavc1gaw')
 .then(res => res.json())
 .then(posts => { 
 console.log(posts)
+    var movieTitle = document.getElementById('movie-title')
+    movieTitle.textContent = posts.items[0].fullTitle
+    var movieCrew = document.getElementById('movie-crew')
+    movieCrew.textContent = posts.items[0].crew
+    var movieRating = document.getElementById('movie-rating')
+    movieRating.textContent = posts.items[0].imDbRating
+    console.log(movieTitle,movieTitle)
+    var movieImg = document.querySelector('#movie-img')
+    movieImg.setAttribute ("src",posts.items[0].image) 
+
 })
 }
 
 movieSearchBtn.addEventListener('click', function(){
-    var movieBtnEl = document.getElementById('movie-placeholder').event
+    var movieBtnEl = document.getElementById('movie-placeholder')
     getMovie(movieBtnEl)
     console.log(movieBtnEl)
 });
