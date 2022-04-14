56510fdb221a62e9805b995af4c12f2fd36b607d
Fiona Carroll <fiona.carroll@me.com>
2022-04-14 13:28:30 -0700

    deleted extra script file

--- script.js
+++
@@ -1,8 +0,0 @@
-
-    fetch('https://imdb-api.com/en/API/Top250Movies/k_pavc1gaw')
-        .then(res => res.json())
-        .then(posts => { 
-            console.log(posts)
-           
-            
-        })
