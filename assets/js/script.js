2b0f4ee83095cd749465b29322fb45c6c6bcd945
lgarciajed <lgarcia186@gmail.com>
2022-04-14 10:15:32 -0700

    testing new javascript

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+var randomRecipeBtn = document.getElementById("randomRecipe");
+
+var getRecipe = (recipe) =>{
+    fetch("https://api.spoonacular.com/recipes/random?apiKey=d9b79695fd2743d2a6de54efb96ffd14")
+    .then(res => res.json())
+    .then(posts => {
+        console.log(posts)
+        var recipeTitle = document.getElementById('recipeResult')
+        recipeTitle.textContent = posts.recipes[0].title
+        var ingredients = posts.recipes[0].extendedIngredients
+        for (var i = 0; i < ingredients.length; i++) {
+            var element = ingredients[i];
+            console.log(element);
+            var foodName = document.getElementById('ingredientResult')
+            foodName.textContent = element.original
+        }
+    })
+}
+
+randomRecipeBtn.addEventListener('click', function(){
+    var recipeBtnEl = document.getElementById('recipeResult')
+    getRecipe(recipeBtnEl)
+    console.log(recipeBtnEl)
+})
