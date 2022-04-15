31e5e8a0fbb6bc6b6d0430e858d350d090f8ac19
Cody Polland <cody.polland@gmail.com>
2022-04-14 19:28:38 -0700

    Movie save feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
 var movieSearchBtn = document.getElementById("moviebtn")
+var movieSaveBtn = document.getElementById('movie-savebtn');
+var movieArray = JSON.parse(localStorage.getItem("movies")) || [];
 
 var getMovie = (movie) => {
     fetch('https://imdb-api.com/en/API/Top250Movies/k_pavc1gaw')
@@ -19,17 +21,31 @@ var getMovie = (movie) => {
             movieRating.textContent = posts.items[i].imDbRating
             var movieImg = document.querySelector('#movie-img')
             movieImg.setAttribute("src", posts.items[i].image)
-
+            movieSaveBtn.addEventListener('click', function(){
+                movieArray.push(posts.items[i])
+                localStorage.setItem("movies", JSON.stringify(movieArray))
+            })
         })
 }
-
+ 
+function displayFav (){
+    var favMoviesContainer = document.getElementById('favorite-movies')
+    for (let i = 0; i < movieArray.length; i++) {
+        var movie = movieArray[i];
+        var div = document.createElement("div")
+        div.setAttribute("class","column mx-3 is-full box has-text-centered")
+        div.textContent = movie.title;
+        favMoviesContainer.append(div)   
+    }
+}
+displayFav()
 
 movieSearchBtn.addEventListener('click', function () {
     var movieBtnEl = document.getElementById('movie-placeholder')
     getMovie(movieBtnEl)
     console.log(movieBtnEl)
 });
-//=====
+
 var randomRecipeBtn = document.getElementById("randomRecipe");
 
 var getRecipe = (recipe) =>{
@@ -37,20 +53,15 @@ var getRecipe = (recipe) =>{
     .then(res => res.json())
     .then(posts => {
         console.log(posts)
-        var foodImage = document.getElementById('food-image')
-        foodImage.setAttribute('src', posts.recipes[0].image)
         var recipeTitle = document.getElementById('recipeResult')
         recipeTitle.textContent = posts.recipes[0].title
         var ingredients = posts.recipes[0].extendedIngredients
         for (var i = 0; i < ingredients.length; i++) {
             var element = ingredients[i];
             console.log(element);
-            var foodName = document.createElement('ol')
-            recipeTitle.append(foodName)
+            var foodName = document.createElement()
             foodName.textContent = element.original
         }
-        var directions = document.getElementById('instruction')
-        directions.innerHTML = posts.recipes[0].instructions
     })
 }
 
